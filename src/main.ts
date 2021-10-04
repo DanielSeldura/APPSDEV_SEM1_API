ad7d1cc53c37a1d114ab34ce23403d81cbbcac58
Daniel Seldura <daniel.seldura@gmail.com>
2021-10-04 10:17:09 +0800

    BASELINE

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
+  await app.listen(3000);
+}
+bootstrap();
