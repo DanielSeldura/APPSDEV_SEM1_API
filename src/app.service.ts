d6270873d7bdbe1c55ebeb46cd35e1b7ca609634
Noel Daniel T. Seldura <daniel.seldura@gmail.com>
2021-10-28 10:54:08 +0800

    update 4

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'AppsDev Server - Non-Firebase Version for Frontend Testing';
   }
 }
